3f4e5902ab9fd5e9de74023fc8358cbba3bb6a7c
Bianca Oliveira <101846059+biaaoliveira@users.noreply.github.com>
2022-09-15 15:46:50 -0300

    atividade de testes

---
+++ todolist/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/modules/categoria.module';
+import { Tarefa } from './tarefa/entities/tarefa.entity';
+import { TarefaModule } from './tarefa/modules/tarefa.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Postal0212/',
+      database: 'db_todo',
+      entities: [Tarefa, Categoria],
+      synchronize: true
+    }),
+    TarefaModule,
+    CategoriaModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
